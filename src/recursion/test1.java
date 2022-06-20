9c3614680c468f79fc59063d6f858feee42c4df4
DOPAMINE-C <462630551@qq.com>
2022-06-20 22:34:45 +0800

    111

--- src/recursion/test1.java
+++ src/recursion/test1.java
@@ -6,6 +6,7 @@ public class test1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        String s = sc.next();
         Deque<Character> stack = new LinkedList<>();
 
         stack.isEmpty();
